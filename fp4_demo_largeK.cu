0d2fd64653ac690bc51c41eb73cfba76ff108094
chenhb <992024760@qq.com>
2025-08-13 10:17:42 +0000

    Fix result error for k = 64

--- fp4_demo_largeK.cu
+++ fp4_demo_largeK.cu
@@ -9,6 +9,7 @@
 #include <iomanip>
 #include <cstdint>
 #include <cmath>
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 // TensorCore05 使用 MXF4NVF4 (4-bit Float) 格式示例
 #define M 128
 #define N 32
@@ -153,14 +154,14 @@ __global__ void mma_on_tmem(uint8_t *mat_a, uint8_t *mat_b, uint8_t *mat_sfa, ui
 
       for (int i = 0; i < M; i++){
         for (int j = 0; j < BLOCKSCALE_NX; j++){
-          sfa_share[(i)*16 + j + (i/32)*4] = mat_sfa[i*16 + (k_loop/16) + j];
+          sfa_share[(i)*16 + j + (i/32)*4] = mat_sfa[i*MAX(16,(K/16)) + (k_loop/16) + j];
         }
-        }
+      }
 
       for (int i = 0; i < M; i+=N){
         for (int j = 0; j < N; j++){
           for (int k = 0; k < BLOCKSCALE_NX; k++){
-            sfb_share[(i+j)*16 + k] = mat_sfb[j*16 + (k_loop/16) + k];
+            sfb_share[(i+j)*16 + k] = mat_sfb[j*MAX(16,(K/16)) + (k_loop/16) + k];
           }
         }
       }
@@ -228,6 +229,7 @@ __global__ void mma_on_tmem(uint8_t *mat_a, uint8_t *mat_b, uint8_t *mat_sfa, ui
 
     // if (tid%32==0) {
 
+    asm volatile ("tcgen05.fence::after_thread_sync;");
     if (tid ==0) {
 
 #if SCALE_FORMAT == 0   // E4M3
@@ -358,11 +360,11 @@ int main() {
     cudnnMallocHostInBytes(&host_A, M * K * sizeof(uint8_t) / 2);
     cudnnMallocHostInBytes(&host_B, N * K * sizeof(uint8_t) / 2);
     cudnnMallocHostInBytes(&host_C, M * N * sizeof(float));
-    cudnnMallocHostInBytes(&host_sfa, M * (K/16) * sizeof(uint8_t));
-    cudnnMallocHostInBytes(&host_sfb, N * (K/16) * sizeof(uint8_t));
+    cudnnMallocHostInBytes(&host_sfa, M * MAX(16,(K/16)) * sizeof(uint8_t));
+    cudnnMallocHostInBytes(&host_sfb, N * MAX(16,(K/16)) * sizeof(uint8_t));
 
-    for(int i = 0; i < M * (K/16); i++) ((uint8_t *)host_sfa)[i] = 0;
-    for(int i = 0; i < N * (K/16); i++) ((uint8_t *)host_sfb)[i] = 0;
+    for(int i = 0; i < M * MAX(16,(K/16)); i++) ((uint8_t *)host_sfa)[i] = 0;
+    for(int i = 0; i < N * MAX(16,(K/16)); i++) ((uint8_t *)host_sfb)[i] = 0;
 
 #if SCALE_FORMAT == 0   // E4M3
     uint8_t e4m3_valid_vals[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f, 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77};
@@ -370,14 +372,14 @@ int main() {
     for(int k = 0; k < (K/16); k+=4){ 
       for (int i = 0; i < M; ++i) {
         for (int j = 0; j < BLOCKSCALE_NX; j++) {
-          ((uint8_t *)host_sfa)[i*16 + k + j] = (e4m3_valid_vals[rand()%120]);
+          ((uint8_t *)host_sfa)[i*MAX(16,(K/16)) + k + j] = (e4m3_valid_vals[rand()%120]);
           // ((uint8_t *)host_sfa)[i*16 + k + j] = (uint8_t)(127);
         }
       }
       //scale B[N* blockscale]:([8,1]\[8,2]\[8,4]\[16,1]\[16,2]\[16,4]......)
       for (int i = 0; i < N; ++i) {
         for (int j = 0; j < BLOCKSCALE_NX; j++) {
-          ((uint8_t *)host_sfb)[i*16 + k + j] = (e4m3_valid_vals[rand()%120]);
+          ((uint8_t *)host_sfb)[i*MAX(16,(K/16)) + k + j] = (e4m3_valid_vals[rand()%120]);
           // ((uint8_t *)host_sfb)[i*16 + k + j] = (uint8_t)(127);
         }
       }
@@ -388,14 +390,14 @@ int main() {
     for(int k = 0; k < (K/16); k+=4){ 
       for (int i = 0; i < M; ++i) {
         for (int j = 0; j < BLOCKSCALE_NX; j++) {
-          ((uint8_t *)host_sfa)[i*16 + k + j] = (uint8_t)(117+rand()%20);
+          ((uint8_t *)host_sfa)[i*MAX(16,(K/16)) + k + j] = (uint8_t)(117+rand()%20);
           // ((uint8_t *)host_sfa)[i*16 + k + j] = (uint8_t)(127);
         }
       }
       //scale B[N* blockscale]:([8,1]\[8,2]\[8,4]\[16,1]\[16,2]\[16,4]......)
       for (int i = 0; i < N; ++i) {
         for (int j = 0; j < BLOCKSCALE_NX; j++) {
-          ((uint8_t *)host_sfb)[i*16 + k + j] = (uint8_t)(117+rand()%20);
+          ((uint8_t *)host_sfb)[i*MAX(16,(K/16)) + k + j] = (uint8_t)(117+rand()%20);
           // ((uint8_t *)host_sfb)[i*16 + k + j] = (uint8_t)(127);
         }
       }
@@ -469,9 +471,9 @@ int main() {
         std::cout <<"scale A (scale_vec::"<<BLOCKSCALE_NX<<"X 、e8m0) 0x:"<<std::endl;
       for (int i = 0; i < M; ++i) {
         std::cout << std::setw(3) <<i<<":[ ";
-        for(int k = 0; k < (K/16); k+=4){
+        for(int k = 0; k < MAX(16,(K/16)); k+=4){
           for (int j = 0; j < BLOCKSCALE_NX; j ++) {
-            printf("0x%02X , ", ((uint8_t *)host_sfa)[i * 16 + k + j]);
+            printf("0x%02X , ", ((uint8_t *)host_sfa)[i * MAX(16,(K/16)) + k + j]);
           }
         }
         std::cout <<"]"<<std::endl;
@@ -480,9 +482,9 @@ int main() {
         std::cout <<"scale B (scale_vec::"<<BLOCKSCALE_NX<<"X 、e8m0) 0x:"<<std::endl;
       for (int i = 0; i < N; ++i) {
         std::cout << std::setw(3) <<i<<":[ ";
-        for(int k = 0; k < (K/16); k+=4){
+        for(int k = 0; k < MAX(16,(K/16)); k+=4){
           for (int j = 0; j < BLOCKSCALE_NX; j ++) {
-            printf("0x%02X , ", ((uint8_t *)host_sfb)[i * 16 + k + j]);
+            printf("0x%02X , ", ((uint8_t *)host_sfb)[i * MAX(16,(K/16)) + k + j]);
           }
         }
         std::cout <<"]"<<std::endl;
@@ -494,12 +496,12 @@ int main() {
     cudaMalloc(&dev_A, M * K * sizeof(uint8_t) / 2);
     cudaMalloc(&dev_B, N * K * sizeof(uint8_t) / 2);
     cudaMalloc(&dev_C, M * N * sizeof(float));
-    cudaMalloc(&dev_sfa, M * (K/16) * sizeof(uint8_t));
-    cudaMalloc(&dev_sfb, N * (K/16) * sizeof(uint8_t));
+    cudaMalloc(&dev_sfa, M * MAX(16,(K/16)) * sizeof(uint8_t));
+    cudaMalloc(&dev_sfb, N * MAX(16,(K/16)) * sizeof(uint8_t));
     checkCUDAErrors(cudaMemcpy(dev_A, host_A, M * K * sizeof(uint8_t) / 2, cudaMemcpyHostToDevice));
     checkCUDAErrors(cudaMemcpy(dev_B, host_B, N * K * sizeof(uint8_t) / 2, cudaMemcpyHostToDevice));
-    checkCUDAErrors(cudaMemcpy(dev_sfa, host_sfa, M * (K/16) * sizeof(uint8_t), cudaMemcpyHostToDevice));
-    checkCUDAErrors(cudaMemcpy(dev_sfb, host_sfb, N * (K/16) * sizeof(uint8_t), cudaMemcpyHostToDevice));
+    checkCUDAErrors(cudaMemcpy(dev_sfa, host_sfa, M * MAX(16,(K/16)) * sizeof(uint8_t), cudaMemcpyHostToDevice));
+    checkCUDAErrors(cudaMemcpy(dev_sfb, host_sfb, N * MAX(16,(K/16)) * sizeof(uint8_t), cudaMemcpyHostToDevice));
     auto status = cudaLaunchKernelEx(&config, mma_on_tmem,
                                      (uint8_t *)dev_A,
                                      (uint8_t *)dev_B,
